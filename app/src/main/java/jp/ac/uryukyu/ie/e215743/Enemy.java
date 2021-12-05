e4c6de10f8f0c755b4bf99776c5af054279d0195
arimeshinnosuke <e215743@ie.u-ryukyu.ac.jp>
2021-12-05 15:35:53 +0900

    修正

--- app/src/main/java/jp/ac/uryukyu/ie/e215743/Enemy.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e215743/Enemy.java
@@ -12,35 +12,10 @@ public class Enemy extends LivingThing{
     public Enemy (String name, int maximumHP, int attack) {
         super(name,maximumHP,attack);
     }
-<<<<<<< HEAD
+
 
     @Override
-=======
-    /**
-     * Heroへ攻撃するメソッド。
-     * attackに応じて乱数でダメージを算出し、hero.wounded()によりダメージ処理を実行。
-     * @param hero 攻撃対象
-     */
-    public void attack(Hero hero){
-        
-            int damage = (int)(Math.random() * attack);
-            if(hitPoint >0){
-            System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.getName(), damage);
-            hero.wounded(damage);
-            }
-            else{
-                System.out.printf("死亡しているので攻撃不可");
-            }  
-        
-    }  
     
-
-    /**
-     * 自身へ攻撃されたときのダメージ処理をするメソッド。
-     * 指定されたダメージを hitPoint から引き、死亡判定を行う。
-     * @param damage 受けたダメージ
-     */
->>>>>>> origin/master
     public void wounded(int damage){
         hitPoint -= damage;
         if( hitPoint < 0 ) {
@@ -49,6 +24,3 @@ public class Enemy extends LivingThing{
         }
     }
 }
-
-    
-
